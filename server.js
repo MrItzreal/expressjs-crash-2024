4a282b4482f61b8a482def5810580ccfc89fb8b2
Israel Santana <is.wsie@gmail.com>
2024-06-21 11:04:35 -0400

    "Learned: While the res.sendFile() method is useful for sending individual files as HTTP responses, its repetitive nature for multiple files makes static middleware a more efficient approach.

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/about", (req, res) => {
-  res.send("About");
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 app.listen(8000, () => console.log(`Server running on port 8000`));
 
-/*Learned that if I change the scripts in my package.json and use:  "dev": "node --watch server.js" and run command "npm run dev"*/
+/*Learned that if I change the scripts in my package.json and use:  "dev": "node --watch server.js" and run command "npm run dev" my server will auto-refresh without Nodemon npm package */
+
+// res.sendFile() method is used to send the contents of a file as the HTTP response to a client's request.  It's particularly useful for serving static files like HTML pages, images, CSS stylesheets, or JavaScript files.
+
+// While res.sendFile() is great for sending individual files, it can become cumbersome when you have many static pages to serve, as you'd need to create a separate route for each one. A better way is using Express Static Middleware.
