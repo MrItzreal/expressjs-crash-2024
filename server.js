4620738e4ee81cecd64f80968bee934080a9a609
Israel Santana <is.wsie@gmail.com>
2024-06-21 12:44:19 -0400

    Learned: .env file is used to store environment variables for your project.
    These variables can be things like API keys, database credentials, or other sensitive information that shouldn't be directly included in your code.
    
    add .env to .gitignore to avoid pushing to repo and add onto package.json  --env-file=.env if you add a port, api key etc.

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require("express");
 const path = require("path");
 
+// You can add a fallback like || 8000 just in case.
+const port = process.env.PORT || 8000;
+
 const app = express();
 
 // setup static folder
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(8000, () => console.log(`Server running on port 8000`));
+let posts = [
+  {
+    id: 1,
+    title: "Post One",
+  },
+  {
+    id: 2,
+    title: "Post Two",
+  },
+  {
+    id: 3,
+    title: "Post Three",
+  },
+];
 
-/* Express Static Middleware: Efficiently serve multiple static files (HTML, CSS, JavaScript, images, etc.) without needing to create individual routes for each file.
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 
-How it works?
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
-1. Configuration: Use `app.use(express.static('public'))` to tell Express that your static files are located in the `public` directory.  
-2. Automatic Serving:  Express automatically handles requests for files within the `public` directory. For example:
-   - A request to `/` will serve `public/index.html`.
-   - A request to `/about` will serve `public/about.html`.
-   - A request to `/style.css` will serve `public/style.css`.
-   
-    When requesting a file, be sure to include its full name (e.g., `/about.html` not just `/about`). 
-    */
+/* .send() vs .json():
+- res.send(): Use this if you need flexibility and might send different types of data in your responses such as:strings, objects, arrays, or even buffers. .
+
+- res.json(): Use this when you are specifically dealing with JSON data (objects or arrays) to ensure the correct content type and make your code more intention-revealing.
+*/
