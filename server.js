967d50dfebfe23925761e15adf02306e699cebb0
Israel Santana <is.wsie@gmail.com>
2024-06-24 08:31:01 -0400

    Learned: got more familiar with status codes and the option to chain them by adding a dot in between.

--- server.js
+++ server.js
@@ -25,40 +25,27 @@ let posts = [
 ];
 
 // Get all posts
+// You can chain on the .status() w/ .json() by adding a dot in between.
 app.get("/api/posts", (req, res) => {
   const limit = parseInt(req.query.limit);
 
   if (!isNaN(limit) && limit > 0) {
-    res.json(posts.slice(0, limit));
+    res.status(200).json(posts.slice(0, limit));
   } else {
-    res.json(posts);
+    res.status(200).json(posts);
   }
 });
 
 // Get single post
 app.get("/api/posts/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  res.json(posts.filter((post) => post.id === id));
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    res.status(404).json({ msg: `A post with the id of ${id} was not found` });
+  } else {
+    res.status(200).json(post);
+  }
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-/* "Get all posts" function demonstrates input sanitization by converting user-provided data into a specific type (integer) and checking its validity before using it, preventing potential errors and security risks.
-
-SQL Injections are DANGEROUS! 
--attackers can exploit SQL injection to gain unauthorized access to your entire database.
--Manipulate Data: can modify and delete data.
--Denial of Service: can make your site unresponsive and crash.
--Server Compromise: might breach your server.
- */
-
-/*req.query is an object that contains the query parameters of an HTTP request. Query parameters are key-value pairs appended to the end of a URL after a question mark (?). They are used to send additional data to the server along with the request. 
-
-https://www.example.com/search?query=expressjs&page=2
-
-query=expressjs and page=2 are query parameters.
-query and page are the keys.
-expressjs and 2 are the corresponding values.
-*/
-
-// query parameters are typically strings, even if you intend them to be numbers.Thus why parseInt() is important to use.
